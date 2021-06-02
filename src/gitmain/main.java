6f20d159bd31d3b05c123e7c80fe24a7f1f245aa
LukaszMichalsky <43893107+LukaszMichalsky@users.noreply.github.com>
2021-06-02 11:10:59 +0200

    zmieniono plik main

---
+++ src/gitmain/main.java
@@ -0,0 +1,9 @@
+package gitmain;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Umiesc mnie na gicie");
+        System.out.println("Dodano mnie na gita");
+
+    }
+}
